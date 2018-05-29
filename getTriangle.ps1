bf9bd37cb1962840f87678679e57359b87d33f52
Jessie Westlake <jessie.westlake@me.com>
2018-05-29 12:39:42 -0400

    Upload getTriangle.ps1 file.
    This is the first iteration of a .ps1 function that is meant to give you the sides of a triangle when you provide the fractional value of a trigonometric function. If only the numerator is given, the assumed denominator is 1. There are serious bugs and incomplete code, but I wanted to get something posted, to overcome my fear of publishing unfinished work (the work is never finished!).

---
+++ getTriangle.ps1
@@ -0,0 +1,35 @@
+﻿Function Get-Triangle
+{
+
+Param(
+    [Parameter(Mandatory=$True)]
+    [ValidateSet("Sin",'Cos','Tan','Cot','Sec','Csc')]
+    $TrigFunction,
+
+    [Parameter(Mandatory=$True)]
+    [int]$Numerator,
+
+    [Parameter(Mandatory=$False)]
+    [int]$Denominator=1
+)
+
+switch ($TrigFunction)
+{
+    'Sin' {$Opposite = $Numerator; $Hypotenuse = $Denominator; $Adjacent = [Math]::Sqrt( [Math]::Pow($Hypotenuse,2) - [Math]::Pow($Opposite,2) )}
+    'Cos' {$Adjacent = $Numerator; $Hypotenuse = $Denominator; $Opposite = [Math]::Sqrt( [Math]::Pow($Hypotenuse,2) - [Math]::Pow($Adjacent,2))}
+    'Tan' {$Opposite = $Numerator; $Adjacent = $Denominator; $Hypotenuse = [Math]::Sqrt( [Math]::Pow($Adjacent,2) + [Math]::Pow($Opposite,2) )}
+    'Csc' {}
+    'Sec' {}
+    'Cot' {}
+
+
+    Default {}
+}
+
+Write-Host -Object "$TrigFunction = $($Numerator/$Denominator)"
+Write-Host -Object "Adjacent: $([Math]::Round($Adjacent,4)) | sqrt($([Math]::Pow($Adjacent,2)))"
+Write-Host -Object "Opposite: $([Math]::Round($Opposite,4)) | sqrt($([Math]::Pow($Opposite,2)))"
+Write-Host -Object "Hypotenuse: $([Math]::Round($Hypotenuse,4)) | sqrt($([Math]::Pow($Hypotenuse,2)))"
+Write-Host -Object "`n"
+
+}
